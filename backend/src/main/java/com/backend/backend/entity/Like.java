9dd549566ee156177273d5c347461880e4a8a86c
binh1922004 <binh1920042018@gmail.com>
2025-02-24 13:37:32 +0700

    add like and reply entity

---
+++ backend/src/main/java/com/backend/backend/entity/Like.java
@@ -0,0 +1,32 @@
+package com.backend.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Likes")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "create_at")
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topic;
+}
