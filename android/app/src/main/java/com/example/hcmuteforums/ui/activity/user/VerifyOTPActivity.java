d10a62e45eba8dc8023092c2b1ca23ca5bf69c09
Trần Trọng Nghĩa <163334466+TranTrongNghia1609@users.noreply.github.com>
2025-02-24 14:45:57 +0700

    OtpActivity

--- android/app/src/main/java/com/example/hcmuteforums/ui/activity/user/VerifyOTPActivity.java
+++ android/app/src/main/java/com/example/hcmuteforums/ui/activity/user/VerifyOTPActivity.java
@@ -1,8 +1,6 @@
 package com.example.hcmuteforums.ui.activity.user;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.hcmuteforums.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class VerifyOTPActivity extends AppCompatActivity {
 
-
-    private TextView tvRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        tvRegister = findViewById(R.id.tvRegister);
-        tvRegister.setOnClickListener(v->{
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity(intent);
+        setContentView(R.layout.activity_verify_otpactivity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
