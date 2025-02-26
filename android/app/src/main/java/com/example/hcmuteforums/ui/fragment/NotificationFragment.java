5389838b82ab9e10bb6064d8400005b756467658
binh1922004 <binh1920042018@gmail.com>
2025-02-26 15:13:09 +0700

    iss: design profile fragment and set bottom navigation button

--- android/app/src/main/java/com/example/hcmuteforums/ui/fragment/NotificationFragment.java
+++ android/app/src/main/java/com/example/hcmuteforums/ui/fragment/NotificationFragment.java
@@ -1,4 +1,4 @@
-package com.example.hcmuteforums;
+package com.example.hcmuteforums.ui.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.hcmuteforums.R;
 
 /**
  * A simple {@link Fragment} subclass.
