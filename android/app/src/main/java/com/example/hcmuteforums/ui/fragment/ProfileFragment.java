867dc50fab8045a5c84b48219c1c05b128b09ba3
binh1922004 <binh1920042018@gmail.com>
2025-02-26 15:19:30 +0700

    iss: add login function when not login

--- android/app/src/main/java/com/example/hcmuteforums/ui/fragment/ProfileFragment.java
+++ android/app/src/main/java/com/example/hcmuteforums/ui/fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.hcmuteforums.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.hcmuteforums.R;
+import com.example.hcmuteforums.ui.activity.user.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +63,13 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button btn = view.findViewById(R.id.btnLogin);
+        btn.setOnClickListener(v -> {
+            Intent myIntent = new Intent(this.getContext(), LoginActivity.class);
+            startActivity(myIntent);
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 }
