cf617fcf057fc66e067413f6c346b2023a4cdd2f
binh1922004 <binh1920042018@gmail.com>
2025-03-01 20:33:52 +0700

    iss: add doc sql

---
+++ doc.sql
@@ -0,0 +1,5 @@
+INSERT INTO hcmuteforums.category (id, description, name) VALUES
+('550e8400-e29b-41d4-a716-446655440000', 'Thông tin và hỗ trợ việc học tập.', 'Thắc mắc học tập'),
+('6f9619ff-8b86-d011-b42d-00c04fc964ff', 'Thông tin về các sự kiện hoạt động xã hội.', 'Hoạt động xã hội'),
+('3fa85f64-5717-4562-b3fc-2c963f66afa6', 'Nơi để có thể tư vấn về lựa chọn công nghệ.', 'Tư vấn công nghệ'),
+('731f65d8-541f-11ec-bf63-0242ac130002', 'Không gian để mọi người có thể mua và bán các món đồ cơ bản.', 'Mua bán');
