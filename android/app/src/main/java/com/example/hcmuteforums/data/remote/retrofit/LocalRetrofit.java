9b592f3ee101a8864f787aa1304ae9b24d3c337f
binh1922004 <binh1920042018@gmail.com>
2025-03-05 14:07:32 +0700

    iss: add login API)

---
+++ android/app/src/main/java/com/example/hcmuteforums/data/remote/retrofit/LocalRetrofit.java
@@ -0,0 +1,32 @@
+package com.example.hcmuteforums.data.remote.retrofit;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LocalRetrofit {
+    private Retrofit retrofit;
+    public LocalRetrofit(Context context){
+        initializeRetrofit(context);
+    }
+
+    private void initializeRetrofit(Context context) {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+//                .addInterceptor(new AuthInterceptor(context))
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8080/ute")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .client(client)
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+}
--- android/app/src/main/java/com/example/hcmuteforums/remote/retrofit/LocalRetrofit.java
+++
@@ -1,4 +0,0 @@
-package com.example.hcmuteforums.remote.retrofit;
-
-public class LocalRetrofit {
-}
