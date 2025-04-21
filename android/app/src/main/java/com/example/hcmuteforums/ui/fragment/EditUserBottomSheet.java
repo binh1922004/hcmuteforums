5b8ddfcd6ba01f140945e961a5914c5acdc33dc5
Trần Trọng Nghĩa <163334466+TranTrongNghia1609@users.noreply.github.com>
2025-04-21 19:22:02 +0700

    UpdateAddress, Dob

--- android/app/src/main/java/com/example/hcmuteforums/ui/fragment/EditUserBottomSheet.java
+++ android/app/src/main/java/com/example/hcmuteforums/ui/fragment/EditUserBottomSheet.java
@@ -1,13 +1,16 @@
 package com.example.hcmuteforums.ui.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,21 +34,39 @@ import com.example.hcmuteforums.viewmodel.AuthenticationViewModel;
 import com.example.hcmuteforums.viewmodel.UserViewModel;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class EditUserBottomSheet extends BottomSheetDialogFragment {
     private ViewGroup containerView;
-
+    UserViewModel userViewModel;
+    AuthenticationViewModel authenticationViewModel;
+    private final Calendar calendar = Calendar.getInstance();
     UserUpdateRequest userUpdateRequest;
     UserUpdateRequest user;
     UserResponse userCurrent;
-    String fullname, dob, gender, address, phone;
+    String fullname, dob, gender, address, phone, dobEdit;
+    ImageView imgClose;
+
+    TextView tv_fullname, tv_dob;
+    TextInputEditText tIEDT_dob, tIEDT_address;
+
+    EditText edt_ho, edt_tendem, edt_ten;
+    Button btn_editDob;
+    String ho = "" , ten="", tendem = "";
+    void anhxa(View view)
+    {
+        imgClose = view.findViewById(R.id.imgClose);
+        tv_fullname = view.findViewById(R.id.tv_Fullname);
+    }
     public static final String ARG_USER = "user";
     public static EditUserBottomSheet newInstance(UserResponse user)
     {
@@ -90,10 +111,11 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView tv_fullname = view.findViewById(R.id.tv_Fullname);
+        anhxa(view);
         if(getArguments()!=null){
             userCurrent = (UserResponse) getArguments().getSerializable("user");
         }
+        userViewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
         fullname = userCurrent.getFullName().toString();
         dob = userCurrent.getDob().toString();
         gender = userCurrent.getGender().toString();
@@ -111,6 +133,10 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
         Log.d("Fullname", fullname);
         tv_fullname.setText(userCurrent.getFullName());
         setupRecyclerView(view);
+        //Event
+        imgClose.setOnClickListener(v -> {
+            dismiss();
+        });
     }
 
 
@@ -127,17 +153,21 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
         TextView tv_fullname = mainView.findViewById(R.id.tv_Fullname);
         tv_fullname.setText(userCurrent.getFullName());
         parent.addView(mainView);
-
         setupRecyclerView(mainView); // Set lại RecyclerView
     }
 
     private void setupRecyclerView(View view) {
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        List<String> items = Arrays.asList("Tên", "Số điện thoại", "Ngày Sinh", "Địa Chỉ", "Giới Tính");
+        List<String> items = Arrays.asList("Tên", "Ngày Sinh", "Địa Chỉ", "Giới Tính");
 
         EditUserAdapter adapter = new EditUserAdapter(items, position -> {
             if (position == 0) {
                 switchToEditNameLayout();
+            }
+            else if(position == 1){
+                switchToViewDobLayout();
+            }else if(position==2){
+                switchToEditAddressLayout();
             }
         });
 
@@ -145,11 +175,6 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), getResources().getColor(android.R.color.darker_gray), 4));
     }
-
-    UserViewModel userViewModel;
-    AuthenticationViewModel authenticationViewModel;
-    EditText edt_ho, edt_tendem, edt_ten;
-    String ho = "" , ten="", tendem = "";
 
     private void getInfo(View mainView, UserUpdateRequest userUpdateRequest)
     {
@@ -180,22 +205,6 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
     }
     private void getProfileInfo(View editNameView)
     {
-       /* String name = fullname;
-        if(name!=null)
-        {
-            Log.d("Name Edit", name);
-
-        }
-        else{
-            Log.d("Name Edit"  , "Null");
-            return;
-        }
-        userUpdateRequest.setFullName(name.toString());
-        userUpdateRequest.setDob(dob);
-        userUpdateRequest.setPhone(phone);
-        userUpdateRequest.setGender(gender);
-        userUpdateRequest.setAddress(address);
-        SetEdtName(editNameView, userUpdateRequest.getFullName());*/
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         userViewModel.getInfo();
         userViewModel.getUserInfo().observe(getViewLifecycleOwner(), new Observer<UserResponse>() {
@@ -210,7 +219,10 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
                             userResponse.getGender()
                     );
                 }
-                SetEdtName(editNameView, userResponse.getFullName());
+                fullname = userResponse.getFullName().toString();
+                dob = userResponse.getDob().toString();
+                dobEdit = userResponse.getDob().toString();
+                address = userResponse.getAddress().toString();
 
             }
         });
@@ -226,6 +238,22 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
 
 
     }
+    private void switchToEditAddressLayout(){
+        View view = getView();
+        if(view==null) return;
+        ViewGroup parent = (ViewGroup) view.findViewById(R.id.bottomSheetContainer);
+        parent.removeAllViews();
+        View editAddress = LayoutInflater.from(getContext()).inflate(R.layout.fragment_bottom_sheet_address, parent, true);
+        getProfileInfo(editAddress);
+        tIEDT_address = (TextInputEditText) editAddress.findViewById(R.id.edt_Address);
+        tIEDT_address.setText(address);
+        Button btn_save = (Button) editAddress.findViewById(R.id.btnSaveAddress);
+        btn_save.setOnClickListener(v->{
+            updateAddress(editAddress, tIEDT_address);
+        });
+
+    }
+
 
     private void NameSplitter(String fullname)
     {
@@ -255,11 +283,186 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
 
         View editNameView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_edit_name, parent, false);
         getProfileInfo(editNameView);
+        SetEdtName(editNameView, fullname);
         //Event
         updateName(editNameView);
         parent.addView(editNameView);
 
+        ImageView backMain = (ImageView) editNameView.findViewById(R.id.btnBack);
+        backMain.setOnClickListener(v->{
+            switchToMainLayout();
+        });
+
     }
+    private void switchToViewDobLayout(){
+        View view = getView();
+        if(view == null) return;
+        ViewGroup parent = (ViewGroup) view.findViewById(R.id.bottomSheetContainer);
+        parent.removeAllViews();
+
+        View viewDob = LayoutInflater.from(getContext()).inflate(R.layout.bottom_sheet_edit_dob, parent, true);
+        getProfileInfo(viewDob);
+        SetTextDob(viewDob, userCurrent.getDob());
+        btn_editDob = (Button) viewDob.findViewById(R.id.btnEditDob);
+        btn_editDob.setOnClickListener(v -> {
+            switchToEditDobLayout(dob);
+        });
+        ImageView backMain = (ImageView) viewDob.findViewById(R.id.btnBack);
+        backMain.setOnClickListener(v->{
+            switchToMainLayout();
+        });
+    }
+    private void switchToEditDobLayout(String dob){
+        View view = getView();
+        if(view == null) return;
+        ViewGroup parent = (ViewGroup) view.findViewById(R.id.dobContainer);
+        parent.removeAllViews();
+        View editDob = LayoutInflater.from(getContext()).inflate(R.layout.fragment_bottom_sheet_edit_dob, parent, true);
+        tIEDT_dob = (TextInputEditText) editDob.findViewById(R.id.edtDob);
+        ImageView back= (ImageView) editDob.findViewById(R.id.btnBack);
+        tIEDT_dob.setText(dob);
+        tIEDT_dob.setOnClickListener(v->{
+            showDatePickerDialog();
+        });
+        Button btn_save = (Button) editDob.findViewById(R.id.btnSaveDob);
+        btn_save.setOnClickListener(v->{
+            updateDob(editDob, tIEDT_dob);
+        });
+        back.setOnClickListener(v->{
+            switchToViewDobLayout();
+        });
+    }
+
+    private void showDatePickerDialog() {
+        String dobText = tIEDT_dob.getText().toString().trim();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+
+        if (!dobText.isEmpty()) {
+            try {
+                Date parsedDate = sdf.parse(dobText);
+                calendar.setTime(parsedDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        new DatePickerDialog(getContext(), (view, year, month, dayOfMonth) -> {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            tIEDT_dob.setText(sdf.format(calendar.getTime()));
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+    private String convertDate(String inputDate){
+        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        // Định dạng đầu ra: Ngày 21, tháng 4, năm 2000
+        SimpleDateFormat outputFormat = new SimpleDateFormat("d 'tháng' M, yyyy", new Locale("vi"));
+        try {
+            Date date = inputFormat.parse(inputDate);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "Không rõ ngày";
+        }
+    }
+
+    private void SetTextDob(View viewDob, String dob){
+        tv_dob = (TextView) viewDob.findViewById(R.id.textDob);
+        String formattedDate = convertDate(dob);
+        tv_dob.setText(formattedDate);
+    }
+    private void updateDob(View editDob, TextInputEditText edt_dob){
+        Button btnSave = editDob.findViewById(R.id.btnSaveDob);
+
+        btnSave.setOnClickListener(view -> {
+            String NewDob = edt_dob.getText().toString().trim();
+            if(NewDob!=null){
+                userUpdateRequest.setDob(NewDob);
+                userViewModel.updateUser(userUpdateRequest);
+                userCurrent.setDob(NewDob);
+                // Quan sát lỗi cập nhật thông tin người dùng
+
+            }
+            userViewModel.getUserUpdateError().observe(getViewLifecycleOwner(), new Observer<Event<Boolean>>() {
+                @Override
+                public void onChanged(Event<Boolean> event) {
+                    Boolean errorOccurred = event.getContent();
+                    if (errorOccurred != null && errorOccurred) {
+                        Toast.makeText(getContext(), "Đã xảy ra lỗi trong quá trình cập nhật thông tin", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            // Quan sát thông báo cập nhật thành công
+            userViewModel.getUserUpdate().observe(getViewLifecycleOwner(), new Observer<Event<Boolean>>() {
+                @Override
+                public void onChanged(Event<Boolean> event) {
+                    Boolean updateSuccess = event.getContent();
+                    if (updateSuccess != null && updateSuccess) {
+                        Toast.makeText(getContext(), "Cập nhật ngày sinh thành công", Toast.LENGTH_SHORT).show();
+                        switchToViewDobLayout();   //Chuyen ve viewDobsheet
+                    }
+                }
+            });
+
+            // Quan sát thông báo lỗi
+            userViewModel.getMessageError().observe(getViewLifecycleOwner(), new Observer<Event<String>>() {
+                @Override
+                public void onChanged(Event<String> event) {
+                    String errorMessage = event.getContent();
+                    if (errorMessage != null) {
+                        Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        });
+    }
+    private void updateAddress(View editAddress, TextInputEditText edt_Address){
+        Button btnSave = editAddress.findViewById(R.id.btnSaveAddress);
+        btnSave.setOnClickListener(view -> {
+            String newAddress = edt_Address.getText().toString();
+            Log.d("New Address", newAddress);
+            Log.d("DEBUG", "userUpdateRequest = " + userUpdateRequest);
+            Log.d("DEBUG", "newAddress = " + newAddress);
+            if(newAddress!=null){
+                userUpdateRequest.setAddress(newAddress);
+                userViewModel.updateUser(userUpdateRequest);
+            }
+        });
+        userViewModel.getUserUpdateError().observe(getViewLifecycleOwner(), new Observer<Event<Boolean>>() {
+            @Override
+            public void onChanged(Event<Boolean> event) {
+                Boolean errorOccurred = event.getContent();
+                if (errorOccurred != null && errorOccurred) {
+                    Toast.makeText(getContext(), "Đã xảy ra lỗi trong quá trình cập nhật thông tin", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Quan sát thông báo cập nhật thành công
+        userViewModel.getUserUpdate().observe(getViewLifecycleOwner(), new Observer<Event<Boolean>>() {
+            @Override
+            public void onChanged(Event<Boolean> event) {
+                Boolean updateSuccess = event.getContent();
+                if (updateSuccess != null && updateSuccess) {
+                    Toast.makeText(getContext(), "Cập nhật địa chỉ thành công", Toast.LENGTH_SHORT).show();
+                    switchToMainLayout();
+                }
+            }
+        });
+
+        // Quan sát thông báo lỗi
+        userViewModel.getMessageError().observe(getViewLifecycleOwner(), new Observer<Event<String>>() {
+            @Override
+            public void onChanged(Event<String> event) {
+                String errorMessage = event.getContent();
+                if (errorMessage != null) {
+                    Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     private void SetEdtName(View editNameView, String fullname)
     {
         //Lay thong tin ra
@@ -278,7 +481,7 @@ public class EditUserBottomSheet extends BottomSheetDialogFragment {
 
 
     private void updateName(View editNameView){
-        Button btnSave = editNameView.findViewById(R.id.btnSave);
+        Button btnSave = editNameView.findViewById(R.id.btnSaveName);
         edt_ho = editNameView.findViewById(R.id.edtHo);
         edt_tendem = editNameView.findViewById(R.id.edtTenDem);
         edt_ten = editNameView.findViewById(R.id.edtTen);
